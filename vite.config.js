f3d765c5f437f01658989505ce9cf8064cae2158
Abhinav Tirole <abhinav.tirole2021@vitbhopal.ac.in>
2024-10-09 14:17:12 +0530

    firebase added

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import vercel from 'vite-plugin-vercel';
- 
+
 export default defineConfig({
   server: {
-    port: 3000,
+    port: process.env.PORT || 3000, // Use the PORT from environment or fallback to 3000
   },
-  plugins: [vercel()],
+  plugins: [react(), vercel()],
 });
